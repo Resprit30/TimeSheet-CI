b2232a3c2ab63c17719f751f5ee246ca73f2ea5f
Resprit30 <rim.ammar@esprit.tn>
2020-11-01 17:36:30 +0100

    update

---
+++ src/main/java/tn/esprit/spring/services/IEmployeService.java
@@ -0,0 +1,41 @@
+package tn.esprit.spring.services;
+
+
+import java.util.List;
+
+
+import tn.esprit.spring.entities.Employe;
+import tn.esprit.spring.entities.Entreprise;
+
+
+
+public interface IEmployeService {
+	
+	public Employe authenticate(String login, String password) ;
+	 
+	
+	public void mettreAjourEmailByEmployeId(String email, int employeId);
+	public void affecterEmployeADepartement(int employeId, int depId);
+	public void desaffecterEmployeDuDepartement(int employeId, int depId);
+	
+	
+	public String getEmployePrenomById(int employeId);
+	public void deleteEmployeById(int employeId);
+	
+	public int getNombreEmployeJPQL();
+	public List<String> getAllEmployeNamesJPQL();
+	public List<Employe> getAllEmployeByEntreprise(Entreprise entreprise);
+	public void mettreAjourEmailByEmployeIdJPQL(String email, int employeId);
+
+	public float getSalaireByEmployeIdJPQL(int employeId);
+	public Double getSalaireMoyenByDepartementId(int departementId);
+	public List<Employe> getAllEmployes();
+	
+
+	int addOrUpdateEmploye(Employe employe);
+	
+	
+	
+
+	
+}
---
+++ src/main/java/tn/esprit/spring/services/IEntrepriseService.java
@@ -0,0 +1,14 @@
+package tn.esprit.spring.services;
+
+import java.util.List;
+
+
+import tn.esprit.spring.entities.Entreprise;
+
+public interface IEntrepriseService {
+	
+	public int ajouterEntreprise(Entreprise entreprise);
+	List<String> getAllDepartementsNamesByEntreprise(int entrepriseId);
+	public void deleteEntrepriseById(int entrepriseId);
+	public Entreprise getEntrepriseById(int entrepriseId);
+}
