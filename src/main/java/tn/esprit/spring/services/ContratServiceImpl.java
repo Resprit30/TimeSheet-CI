061ba7972113455d38d329232e5d74b2675131d5
Aya Bouzazi <ayabouzazi021@gmail.com>
2020-11-02 22:33:50 +0100

    log4j

--- src/main/java/tn/esprit/spring/services/ContratServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/ContratServiceImpl.java
@@ -2,6 +2,9 @@ package tn.esprit.spring.services;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,33 +22,62 @@ public class ContratServiceImpl implements IContratService {
 	@Autowired
 	EmployeRepository employeRepository;
 
+	private static final Logger l = LogManager.getLogger(ContratServiceImpl.class);
 
 	public List<Contrat> getAllContrats() {
+		l.info("In getAllContrats() : ");
+		
+		l.debug("Accéss à la base de données");
+		l.debug("Récupération de la liste des contrats");
+		
+		l.info("Out getAllContrats() ");
 		return (List<Contrat>) contratRepository.findAll();
 	}
 
-	public int ajouterContrat(Contrat contrat) {
-		contratRepository.save(contrat);
-		return contrat.getReference();
+	public Integer ajouterContrat(Contrat contrat) {
+		l.debug("In ajouterContrat");
+		try{
+			contratRepository.save(contrat);
+			l.info("Contrat ajouter avec ref = "+contrat.getReference());
+			l.debug("Out ajouterContrat");
+			return contrat.getReference();
+		} catch (Exception e) {
+			l.error("erreur dans la methode ajouterContrat :"+e);
+			return null;
+		}
 	}
 
 	public void affecterContratAEmploye(int contratId, int employeId) {
 		Contrat contratManagedEntity = contratRepository.findById(contratId).orElse(null);
+		l.debug("Contrat par identifiant");
 		Employe employeManagedEntity = employeRepository.findById(employeId).orElse(null);
-
+		l.debug("Employe par identifiant");
+		
 		if(contratManagedEntity!=null){
 		contratManagedEntity.setEmploye(employeManagedEntity);
+		l.info("L'affectation a été faite");
 		contratRepository.save(contratManagedEntity);
 		}
 	}
 
-public void deleteContratById(int contratId) {
-		Contrat contratManagedEntity = contratRepository.findById(contratId).orElse(null);
+public int deleteContratById(int contratId) {
+    l.debug("In deleteContratById ");
+    try {
+    	Contrat contratManagedEntity = contratRepository.findById(contratId).get();
+    	l.info("Ce contrat a été supprimer avec succes");
 		contratRepository.delete(contratManagedEntity);
+		return 0;
+    }catch (Exception e){
+    	l.error("erreur methode deleteContratById :"+e);
+    	return -1;
+    	
+    }
 
 	}
 	public void deleteAllContratJPQL() {
+		l.debug("In deleteAllContratJPQL ");
 		employeRepository.deleteAllContratJPQL();
+		l.info("Liste de contrats a été supprimer");
 	}
 
 }
