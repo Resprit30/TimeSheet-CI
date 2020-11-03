0828fb618c42996b728807cac2d5bec70719b2dc
Aya Bouzazi <ayabouzazi021@gmail.com>
2020-11-03 01:22:15 +0100

    update

--- src/main/java/tn/esprit/spring/services/IContratService.java
+++ src/main/java/tn/esprit/spring/services/IContratService.java
@@ -9,9 +9,9 @@ public interface IContratService {
 	
 	
 	public List<Contrat> getAllContrats();
-	public int ajouterContrat(Contrat contrat);
+	public Integer ajouterContrat(Contrat contrat);
 	public void affecterContratAEmploye(int contratId, int employeId);
-	public void deleteContratById(int contratId);
+	public int deleteContratById(int contratId);
 	public void deleteAllContratJPQL();
 
 	
