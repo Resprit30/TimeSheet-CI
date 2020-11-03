1913ab79e0d7f19f027d1ce0ed863ca2d546c1a8
Aya Bouzazi <ayabouzazi021@gmail.com>
2020-11-03 02:55:28 +0100

    Junit adding tests

--- src/test/java/tn/esprit/spring/ContratServiceImplTest.java
+++ src/test/java/tn/esprit/spring/ContratServiceImplTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import tn.esprit.spring.entities.Contrat;
 import tn.esprit.spring.entities.Employe;
+import tn.esprit.spring.repository.ContratRepository;
 import tn.esprit.spring.repository.EmployeRepository;
 import tn.esprit.spring.services.IContratService;
 
@@ -25,6 +26,9 @@ public class ContratServiceImplTest {
 	IContratService cs;
 	@Autowired
 	EmployeRepository employeRepository;
+	@Autowired
+	ContratRepository contratRepository;
+	
 	@Test
 	public void testGetAllContrats(){
 		List<Contrat> lc =cs.getAllContrats();
@@ -44,9 +48,19 @@ public class ContratServiceImplTest {
 		int idC = 5;
 		int i=cs.deleteContratById(idC);
 		assertEquals(0,i);
-		
 	}
 	
-
+	@Test
+	public void testAffecterContratAEmploye() {
+		int employeId = 1 ;
+		int idC = 6; 
+		Contrat C = contratRepository.findById(idC).orElse(null);
+		Employe Emp = employeRepository.findById(employeId).orElse(null);
+		if (C != null){
+		C.setEmploye(Emp);
+		Contrat c = cs.affecterContratAEmploye(idC, employeId);
+		Assert.assertEquals (C.getEmploye().getId(),c.getEmploye().getId());
+		}	
+	}
 	
 }
